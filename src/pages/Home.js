2cff5e51f94e0e5472ec7f4b663f08ba552ac115
Joe Langston <joelangston81@gmail.com>
2020-09-08 18:25:46 -0500

    nothing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,8 +8,10 @@ class Home extends React.Component {
     return (
       <div className="Home">
         <Menu />
-        <h2>Your favorite microblogging platform</h2>
+        <h2>Social Distancing Style Blogging</h2>
         <LoginForm />
+        
+    
       </div>
     );
   }
